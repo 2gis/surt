8e3a8022a85bb5c5aca70f7a90bc947383a314cc
Vladislav Sapozhnikov <me@lastw.net>
2016-10-31 15:37:25 +0700

    do not submit incomplete query

--- surt.js
+++ surt.js
@@ -182,18 +182,7 @@ var
                     // Используем isControlKey вместо affectsToValue, так как клавиши delete и backspace
                     // не должны влиять на выбор элемента из саггеста
                     if (self.suggest && self.suggest[self._activeSuggest] && !isControlKey(key)) {
-                        var text = self.text(),
-                            newChar = text.charAt(text.length - 1);
-
-                        if (newChar == ' ') { // Если передан пробел, второй пробел не требуется
-                            newChar = '';
-                        }
                         pickSuggest(false, e);
-
-                        var newText = self.text() + self.delimiter + ' ' + newChar;
-
-                        self.text(newText);
-                        self.restoreCursor(newText.length);
                     }
 
                     self.updateInput();
@@ -228,7 +217,7 @@ var
                     if (key == 13) {
                         e.preventDefault();
                         var submit = true;
-                        if (self._activeSuggest != -1 && self.suggest[self._activeSuggest][0].incomplete_query) {
+                        if (self.isIncompleteQuery()) {
                             submit = false;
                         }
 
@@ -347,9 +336,10 @@ var
 
                     if (!self.suggest || !self.suggest[index]) return; // html illegal append situation
 
-                    var willSubmit = $.inArray('click', self._submitEvents) != -1;
-
                     self._activeSuggest = index;
+
+                    var willSubmit = $.inArray('click', self._submitEvents) != -1 && !self.isIncompleteQuery();
+
                     pickSuggest(willSubmit, e);
                     if (self.params.submit && willSubmit) { // Если пользователь хочет, то клик по сагесту приведёт к поиску, и обновлять сагест уже не надо
                         self.params.submit(e, true);
@@ -466,6 +456,11 @@ var
                 }
 
                 inputHTML = inputHTML.join(this.delimiter + ' ');
+                
+                if (this.isIncompleteQuery() && inputHTML[inputHTML.length - 1] != ' ') {
+                    inputHTML += ' ';
+                }
+
                 // if (this.trailingSpace) inputHTML += space;
                 if (this.params.inputMode != 'text' || force) {
                     this.html(inputHTML);
@@ -636,6 +631,17 @@ var
             }
 
             return spaces(text);
+        },
+
+        isIncompleteQuery: function() {
+            if (!this.suggest ||
+                !this.suggest[this._activeSuggest] ||
+                !this.suggest[this._activeSuggest][0] ||
+                !this.suggest[this._activeSuggest][0].item) {
+                return false;
+            }
+
+            return this.suggest[this._activeSuggest][0].item.hint.incomplete_query;
         },
 
         // Возвращает или устанавливает текст в инпут
